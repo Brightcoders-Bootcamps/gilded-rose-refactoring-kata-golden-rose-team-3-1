f3f576d93d5d755eceabc287128d549b56b5ed70
PaoSantollo <44455124+PaoSantollo@users.noreply.github.com>
2020-07-14 19:18:09 -0500

    Implementation of decorator

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,80 +1,23 @@
 # frozen_string_literal: true
 
+$goods = { aged: "Aged Brie",
+           backstage: "Backstage passes to a TAFKAL80ETC concert",
+           sulfuras: "Sulfuras, Hand of Ragnaros" }
+
 # clase Gilded Rose
 class GildedRose
-  
   def initialize(items)
     @items = items
-    @goods = {  aged: 'Aged Brie',
-                backstage: 'Backstage passes to a TAFKAL80ETC concert',
-                sulfuras: 'Sulfuras, Hand of Ragnaros' }
-  end
-
-  
-  def update_sell_in(item)
-    name = item.name
-    return unless name != @goods[:sulfuras]
-
-    item.sell_in -= 1
-  end
-
-  def outlier_sell_in(item)
-    quality = item.quality
-    return unless item.sell_in.negative?
-
-    if item.name != @goods[:aged]
-      detect_backstage(item)
-    elsif quality < 50
-      item.quality += 1
-    end
-  end
-
-  def detect_backstage(item)
-    item.name != @goods[:backstage] ? item.decrease_quality : item.quality -= item.quality
-  end
-
-  def decrease_quality(item)
-    quality = item.quality
-    return unless quality.positive? && item.name != @goods[:sulfuras]
-
-    item.quality -= 1
-  end
-
-  def define_quality(item)
-    name = item.name
-    if name != @goods[:aged] && name != @goods[:backstage]
-      decrease_quality(item)
-    else
-      increase_quality(item)
-    end
-  end
-
-  def increase_quality(item)
-    return unless item.quality < 50
-
-    item.quality += 1
-    return unless item.name == @goods[:backstage]
-
-    sell_in = item.sell_in
-    increase_backstage(item) if sell_in < 11
-    increase_backstage(item) if sell_in < 6
-  end
-
-  def increase_backstage(item)
-    return unless item.quality < 50
-
-    item.quality += 1
   end
 
   def init_proccess
     @items.each do |item|
-      define_quality(item)
-      update_sell_in(item)
-      outlier_sell_in(item)
+      item.define_quality
+      item.update_sell_in
+      item.outlier_sell_in
     end
   end
 end
-
 
 # Clase Items, no modificar
 class Item
@@ -91,11 +34,56 @@ class Item
   end
 end
 
-module ItemDecorator 
+# Decorator of Item class
+module ItemDecorator
   def decrease_quality
-    return unless @quality.positive? && @name != 'Sulfuras, Hand of Ragnaros'
+    return unless @quality.positive? && @name != $goods[:sulfuras]
 
     @quality -= 1
+  end
+
+  def update_sell_in
+    return unless @name != $goods[:sulfuras]
+
+    @sell_in -= 1
+  end
+
+  def outlier_sell_in
+    return unless @sell_in.negative?
+
+    if @name != $goods[:aged]
+      detect_backstage
+    elsif @quality < 50
+      @quality += 1
+    end
+  end
+
+  def detect_backstage
+    @name != $goods[:backstage] ? decrease_quality : @quality -= @quality
+  end
+
+  def define_quality
+    if @name != $goods[:aged] && @name != $goods[:backstage]
+      decrease_quality
+    else
+      increase_quality
+    end
+  end
+
+  def increase_quality
+    return unless @quality < 50
+
+    @quality += 1
+    return unless @name == $goods[:backstage]
+
+    increase_backstage if @sell_in < 11
+    increase_backstage if @sell_in < 6
+  end
+
+  def increase_backstage
+    return unless @quality < 50
+
+    @quality += 1
   end
 end
 
